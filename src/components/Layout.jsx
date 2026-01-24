cd9006d2b08de2ab86000a58777174426e060213
Sam J <samjojames@gmail.com>
2026-01-24 10:10:12 +0000

    new logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,19 @@ import { VerseSection } from './VerseSection'
 import { QuestionSection } from './QuestionSection'
 import { PrayerSection } from './PrayerSection'
 import { Controls } from './Controls'
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.jpg'
 
 export function Layout({ data, nextChapterId, nextVerseId }) {
     return (
         <main className="max-w-2xl mx-auto min-h-screen flex flex-col">
-            <header className="p-4 md:p-6 text-center flex flex-col items-center gap-2">
-                <img src={logo} alt="Woodgreen Church" className="h-8 md:h-10 object-contain opacity-80" />
+            <header className="p-8 md:p-10 text-center flex flex-col items-center gap-4">
+                <div className="h-24 md:h-28 aspect-square rounded-full overflow-hidden mix-blend-multiply flex items-center justify-center">
+                    <img
+                        src={logo}
+                        alt="Woodgreen Church"
+                        className="w-full h-full object-cover scale-[1.01]"
+                    />
+                </div>
                 <h1 className="font-serif text-xl text-gray-400 italic">One One One</h1>
             </header>
 
